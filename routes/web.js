e23d795acaa046c9ff26f233b2d566f8f854559c
sumit bharodiya <sumitkumarbharodiya@gmail.com>
2021-04-02 18:45:35 +0530

    commit

--- routes/web.js
+++ routes/web.js
@@ -1,3 +1,4 @@
+// Controllers.
 const homeController = require("../app/http/controllers/homeController");
 const authController = require("../app/http/controllers/authController");
 const cartController = require("../app/http/controllers/customers/cartController");
